4b7884324cc8c264952e67af8bf13ead1d2fa8c1
ravikannan <ravi@teambanjo.com>
2020-08-13 18:24:52 -0600

    yolov4 refactor classes

--- tool/torch_utils.py
+++ tool/torch_utils.py
@@ -39,22 +39,7 @@ def bbox_ious(boxes1, boxes2, x1y1x2y2=True):
     return carea / uarea
 
 
-def get_region_boxes(boxes_and_confs):
-    # print('Getting boxes from boxes and confs ...')
 
-    boxes_list = []
-    confs_list = []
-
-    for item in boxes_and_confs:
-        boxes_list.append(item[0])
-        confs_list.append(item[1])
-
-    # boxes: [batch, num1 + num2 + num3, 1, 4]
-    # confs: [batch, num1 + num2 + num3, num_classes]
-    boxes = torch.cat(boxes_list, dim=1)
-    confs = torch.cat(confs_list, dim=1)
-
-    return [boxes, confs]
 
 
 def convert2cpu(gpu_matrix):
